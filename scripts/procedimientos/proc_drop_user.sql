12c36d3010aaa3881303ceb9c28556129b7afd66
adr97rich <adr97rich@gmail.com>
2021-05-10 20:09:26 -0400

    Procedures crear y eliminar usuario

---
+++ scripts/procedimientos/proc_drop_user.sql
@@ -0,0 +1,45 @@
+SET SERVEROUTPUT ON
+SET ECHO OFF
+SET VERIFY OFF
+
+ACCEPT name CHAR PROMPT 'Ingrese nombre del usuario a eliminar: '
+
+CREATE OR REPLACE PROCEDURE Drop_User (user_name varchar) 
+AS
+  SQL1 varchar(100);
+  SQL2 varchar(100);
+  USER_NO_EXIST exception;
+  num_users integer;
+BEGIN
+
+  SELECT COUNT(*) INTO num_users FROM all_users WHERE username=UPPER(user_name);
+
+  IF (num_users = 0) THEN
+    RAISE USER_NO_EXIST;
+  END IF;
+
+  SQL1 := 'ALTER SESSION SET "_ORACLE_SCRIPT" = true';
+  SQL2 := 'DROP USER ' || user_name;
+
+  EXECUTE IMMEDIATE SQL1;
+  EXECUTE IMMEDIATE SQL2;
+
+  DBMS_OUTPUT.PUT_LINE('Usuario eliminado!');
+
+  EXCEPTION
+  WHEN USER_NO_EXIST THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR: Nombre de usuario "' || user_name || '" no existe!');
+
+END Drop_User;
+/
+
+DECLARE
+
+  user_name varchar(20) := '&name';
+
+BEGIN
+
+  Drop_User(user_name);
+  
+END;
+/
