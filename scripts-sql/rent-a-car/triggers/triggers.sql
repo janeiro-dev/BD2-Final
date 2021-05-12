7286a0aec828f10642fd6e3ab3d7b8a4f249d368
Junior <jleandro0709@gmail.com>
2021-05-12 16:51:19 -0400

    borrar tabla de prueba del trigger

--- scripts-sql/rent-a-car/triggers/triggers.sql
+++ scripts-sql/rent-a-car/triggers/triggers.sql
@@ -1,15 +1,13 @@
 -- cuando se crea un reporte de averia sobre un vehiculo, si su prioridad es alta, cambiar estado del vehiculo a mantenimiento
-create or replace TRIGGER enviar_a_mantenimiento
+CREATE OR REPLACE TRIGGER enviar_a_mantenimiento
     BEFORE UPDATE OR INSERT ON reportes_de_averias
     FOR EACH ROW
     WHEN (NEW.prioridad > 1)
 DECLARE
-    -- pragma autonomous_transaction;
     prioridad NUMBER;
     id_vehiculo NUMBER;
 BEGIN
     UPDATE vehiculos v SET v.estado = 'mantenimiento' WHERE v.id_vehiculo = :NEW.id_vehiculo;
-    insert into temporal values (:NEW.id_vehiculo, :NEW.prioridad);
 END;
 
 -- cuando se añada un vehiculo a la tabla mantenimiento, su estado de cambiar a mantenimiento
