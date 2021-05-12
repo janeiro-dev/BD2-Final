1711d0787957b9ff0db238fd0e3226022fbf6544
adr97rich <adr97rich@gmail.com>
2021-05-12 19:02:54 -0400

    Fix procedure y funcion de reportes

--- scripts-sql/rent-a-car/funciones/func_check_vehicle_exist.sql
+++ scripts-sql/rent-a-car/funciones/func_check_vehicle_exist.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION Check_Vehicle_Exist (num_serie in vehiculos.numero_serie%type)
+CREATE OR REPLACE FUNCTION Check_Vehicle_Exist (id in vehiculos.id_vehiculo%type)
 RETURN boolean AS
   num_vehicles integer;
 BEGIN
 
   -- Verificar si vehiculo existe --
 
-  SELECT COUNT(*) INTO num_vehicles FROM vehiculos WHERE vehiculos.numero_serie=num_serie;
+  SELECT COUNT(*) INTO num_vehicles FROM vehiculos WHERE vehiculos.id_vehiculo=id;
 
   IF (num_vehicles > 0) THEN
     RETURN true;
--- scripts-sql/rent-a-car/procedimientos/proc_new_damage_report.sql
+++ scripts-sql/rent-a-car/procedimientos/proc_new_damage_report.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE New_Damage_Report (
-  id_cliente in clientes.id_cliente%type,
+  num_ident in clientes.numero_identificacion%type,
   id_vehiculo in vehiculos.id_vehiculo%type,
   descripcion in reportes_de_averias.descripcion%type,
   prioridad in reportes_de_averias.prioridad%type
@@ -10,21 +10,18 @@ AS
   new_id_reporte reportes_de_averias.id_reporte%type;
   estado reportes_de_averias.estado%type;
   fecha reportes_de_averias.fecha%type;
-  num_ident clientes.numero_identificacion%type;
-  num_serie vehiculos.numero_serie%type;
+  cliente_id clientes.id_cliente%type;
 BEGIN
 
   -- Verificar que cliente exista --
-  SELECT numero_identificacion INTO num_ident FROM clientes WHERE clientes.id_cliente=id_cliente;
-
   IF Check_Client_Exist(num_ident) = false THEN
     RAISE CLIENT_NO_EXIST;
+  ELSE
+    SELECT id_cliente INTO cliente_id FROM clientes WHERE clientes.numero_identificacion=num_ident;
   END IF;
 
   -- Verificar que vehiculo exista --
-  SELECT numero_serie INTO num_serie FROM vehiculos WHERE vehiculos.id_vehiculo=id_vehiculo;
-
-  IF Check_Vehicle_Exist(num_serie) = false THEN
+  IF Check_Vehicle_Exist(id_vehiculo) = false THEN
     RAISE VEHICLE_NO_EXIST;
   END IF;
 
@@ -39,7 +36,7 @@ BEGIN
 
   -- Insertar reporte en tabla Reportes de Averias --
   INSERT INTO reportes_de_averias 
-  VALUES(new_id_reporte, id_cliente, id_vehiculo, descripcion, estado, fecha, prioridad);
+  VALUES(new_id_reporte, cliente_id, id_vehiculo, descripcion, estado, fecha, prioridad);
 
   DBMS_OUTPUT.PUT_LINE(chr(10) || 'Reporte de averia realizado!');
 
