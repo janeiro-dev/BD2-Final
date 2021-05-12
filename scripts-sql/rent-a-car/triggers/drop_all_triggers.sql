41b77e93ce915f2b306000722f66a1aa115ea9a0
Junior <jleandro0709@gmail.com>
2021-05-12 19:53:44 -0400

    add file to drop all triggers

---
+++ scripts-sql/rent-a-car/triggers/drop_all_triggers.sql
@@ -0,0 +1,9 @@
+DROP TRIGGER enviar_a_mantenimiento;
+
+DROP TRIGGER evitar_borar_reportes_de_averias;
+
+DROP TRIGGER evitar_borar_registros_rentas;
+
+DROP TRIGGER actualizar_kilometraje;
+
+DROP TRIGGER verificar_kilometraje;
