dc8b916df0ff764593fa7d511ab895fbb0c40aa1
adr97rich <adr97rich@gmail.com>
2021-05-12 20:18:47 -0400

    Crear archivos drop_all, funciones y procedures

---
+++ scripts-sql/rent-a-car/funciones/drop_all_functions.sql
@@ -0,0 +1,7 @@
+DROP FUNCTION Check_Address_Exist;
+DROP FUNCTION Check_Client_Exist;
+DROP FUNCTION Check_Location_Exist;
+DROP FUNCTION Check_Report_Exist;
+DROP FUNCTION Check_User_Exist;
+DROP FUNCTION Check_Vehicle_Exist;
+DROP FUNCTION Get_Current_Date;
