cca6f93ca54a4fe8969bd32b467e4ea07ae24481
Junior <jleandro0709@gmail.com>
2021-05-12 21:45:53 -0400

    arreglar archivos para la creacion de tablas en base de datos HR

--- scripts-sql/hr/puestos_trabajo.sql
+++ scripts-sql/hr/puestos_trabajo.sql
@@ -1,56 +1,50 @@
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 001,
 'GERENTE',
 78600,
-'Encargado de la parte gerencial/administrativa de la empresa',
-10);
+'Encargado de la parte gerencial/administrativa de la empresa');
 
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 002,
 'DIRECTOR',
 108500,
-'Este tiene la direcci�n superior de una empresa',
-10);
+'Este tiene la direcci�n superior de una empresa');
 
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 003,
 'RECEPCIONISTA',
 30000,
-'Este es engargada de atender llamadas,recibir a los clientes y proveer informacion',
-20);
+'Este es engargada de atender llamadas,recibir a los clientes y proveer informacion');
 
 
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 004,
 'CONDUCTORES',
 26000,
-'Es una persona encargada conducir y transportar vehiculos y demas',
-30);
+'Es una persona encargada conducir y transportar vehiculos y demas');
 
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 005,
 'LAVADORES_DE_VEHICULOS',
 17000,
-'Es una persona encargada de lavar los vehiculos',
-30);
+'Es una persona encargada de lavar los vehiculos');
 
 insert into puestos_trabajo
-(id_puesto_trabajo,nombre,salario,descripcion,id_beneficio)
+(id_puesto_trabajo,nombre,salario,descripcion)
 VALUES(
 006,
 'ENCARGADOS_DE_LIMPIEZA',
 19000,
-'Es una persona encargada de la higiene del la planta fisica',
-30);
+'Es una persona encargada de la higiene del la planta fisica');
 
 
